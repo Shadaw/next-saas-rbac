2922109e47af7f36af7734d8f3faae8386165139
Gustavo Alexandre <gustavoalprofissional@gmail.com>
2026-02-01 09:35:45 -0300

    feat: subjects typing

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,19 +1,12 @@
-import type { CreateAbility, ForcedSubject, MongoAbility } from '@casl/ability'
+import type { CreateAbility, MongoAbility } from '@casl/ability'
 import { AbilityBuilder, createMongoAbility } from '@casl/ability'
 
 import type { User } from './models/user'
 import { permissions } from './permissions'
+import type { ProjectSubject } from './subjects/project'
+import type { UserSubject } from './subjects/user'
 
-const actions = ['manage', 'invite', 'delete'] as const
-const subjects = ['User', 'all'] as const
-
-type AppAbilities = [
-  (typeof actions)[number],
-  (
-    | (typeof subjects)[number]
-    | ForcedSubject<Exclude<(typeof subjects)[number], 'all'>>
-  ),
-]
+type AppAbilities = UserSubject | ProjectSubject | ['manage', 'all']
 
 export type AppAbility = MongoAbility<AppAbilities>
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
